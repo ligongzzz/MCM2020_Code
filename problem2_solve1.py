d825d9b93cd21b3a7b3be06fc2ae9133f26609fd
谢哲 <xiezhe20001128@163.com>
2020-02-16 23:28:16 +0800

    Add AHP Module.

--- problem2_solve1.py
+++ problem2_solve1.py
@@ -4,9 +4,29 @@ import cv2
 import csv
 
 # Hyper parameters.
-
 is_huskies: bool = True
 matchid = 1
+
+# AHP Module.
+RI_dict = {1: 0, 2: 0, 3: 0.58, 4: 0.90,
+           5: 1.12, 6: 1.24, 7: 1.32, 8: 1.41, 9: 1.45}
+
+
+def get_w(array):
+    row = array.shape[0]
+    a_axis_0_sum = array.sum(axis=0)
+    b = array / a_axis_0_sum
+    b_axis_1_sum = b.sum(axis=1)
+    w = b_axis_1_sum / row
+    AW = (w * array).sum(axis=1)
+    max_max = sum(AW / (row * w))
+    CI = (max_max - row) / (row - 1)
+    CR = CI / RI_dict[row]
+    if CR < 0.1:
+        return True, w
+    else:
+        return False, w
+
 
 # Read the match Result file.
 match_file = list(csv.reader(open('./data/matches.csv')))
