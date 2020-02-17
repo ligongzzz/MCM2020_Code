9cda515f88d31a2ebca5f9ebef0dbf8be8020707
谢哲 <xiezhe20001128@163.com>
2020-02-17 10:05:57 +0800

    Update.

---
+++ AHP_utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+# AHP Module.
+RI_dict = {1: 0, 2: 0, 3: 0.58, 4: 0.90,
+           5: 1.12, 6: 1.24, 7: 1.32, 8: 1.41, 9: 1.45}
+
+
+def AHP_Check(array):
+    '''
+    Check if the matrix is available for AHP.
+    '''
+    row = array.shape[0]
+    a_axis_0_sum = array.sum(axis=0)
+    b = array / a_axis_0_sum
+    b_axis_1_sum = b.sum(axis=1)
+    w = b_axis_1_sum / row
+    AW = (w * array).sum(axis=1)
+    max_max = sum(AW / (row * w))
+    CI = (max_max - row) / (row - 1)
+    CR = CI / RI_dict[row]
+    if CR < 0.1:
+        return w
+    else:
+        return None
+
+
+def normalize(input: np.ndarray):
+    mean = input.mean()
+    std = input.std()
+    ans = (input - mean) / std / 2 + 1.0
+    ans = ans * (ans > 0)
+    return ans
+
+
+# Judgement Matrixs.
+ALL = np.array([
+    [1, 1, 3, 2, 2],
+    [1, 1, 2, 2, 2],
+    [1 / 3, 1 / 2, 1, 2, 1],
+    [1 / 2, 1 / 2, 1 / 2, 1, 1 / 2],
+    [1 / 2, 1 / 2, 1, 1 / 2, 1]
+])
+
+OP = np.array([
+    [1, 3, 1, 1, 1, 1 / 2],
+    [1 / 3, 1, 1 / 3, 1 / 3, 1 / 3, 1 / 7],
+    [1, 3, 1, 1, 1, 1 / 2],
+    [1, 3, 1, 1, 1, 1 / 2],
+    [1, 3, 1, 1, 1, 1 / 3],
+    [2, 7, 2, 2, 3, 1]
+])
+
+OA = np.array([
+    [1, 1, 1 / 2],
+    [1, 1, 1 / 2],
+    [2, 2, 1]
+])
+
+DA = np.array([
+    [1, 1, 1 / 2],
+    [1, 1, 1 / 3],
+    [2, 3, 1]
+])
--- problem2_solve1.py
+++ problem2_solve1.py
@@ -2,36 +2,40 @@
 import numpy as np
 import cv2
 import csv
+import AHP_utils
 
 # Hyper parameters.
 is_huskies: bool = True
 matchid = 1
 
-# AHP Module.
-RI_dict = {1: 0, 2: 0, 3: 0.58, 4: 0.90,
-           5: 1.12, 6: 1.24, 7: 1.32, 8: 1.41, 9: 1.45}
-
-
-def get_w(array):
-    row = array.shape[0]
-    a_axis_0_sum = array.sum(axis=0)
-    b = array / a_axis_0_sum
-    b_axis_1_sum = b.sum(axis=1)
-    w = b_axis_1_sum / row
-    AW = (w * array).sum(axis=1)
-    max_max = sum(AW / (row * w))
-    CI = (max_max - row) / (row - 1)
-    CR = CI / RI_dict[row]
-    if CR < 0.1:
-        return True, w
-    else:
-        return False, w
-
-
 # Read the match Result file.
 match_file = list(csv.reader(open('./data/matches.csv')))
 action_file = list(csv.reader(open('./data/fullevents.csv')))[1:]
 csv_reader = list(csv.reader(open('./data/passingevents.csv')))[1:]
+
+# Storage all the data.
+offensive_cnt = np.zeros(76)
+
+
+def get_storage_id(matchid, is_huskies):
+    '''
+    Transform the match ID and is_huskies to the storage_id.
+    '''
+    if is_huskies:
+        return (matchid - 1) * 2
+    else:
+        return matchid * 2 - 1
+
+
+def get_offensive_id(storage_id, index):
+    '''
+    Return the offensive id in the offensive vector.
+    '''
+    cur_cnt = 0
+    for i in range(storage_id):
+        cur_cnt += offensive_cnt[i]
+    return cur_cnt + index
+
 
 while matchid <= 38:
     passing_list = None
@@ -161,10 +165,10 @@ while matchid <= 38:
                     simplepass += 1
                 elif event[7] == 'Smart pass':
                     smartpass += 1
-                elif event[6] == 'Foul' and i+1 < len(event_before) and event_before[i + 1][6] == 'Free Kick':
-                    num3 += 1
                 elif event[7] == 'Corner':
                     flag = 1
+            elif event[6] == 'Foul' and i + 1 < len(event_before) and event_before[i + 1][6] == 'Free Kick':
+                num3 += 1
         time_rate = ball_time / total_time
 
         num5 = 1 if event_after is not None and event_after[6] == 'Save attempt' else 0
@@ -198,8 +202,7 @@ while matchid <= 38:
     else:
         goal_cnt = int(match_file[matchid][4])
     PPDA = PPDA_cnt / PPDA_action
-    defence_val = 1 / ODC + 1 / PPDA - goal_cnt / shot_cnt + clear_cnt - save_cnt
-
+    defence_val = 1 / ODC + 1 / PPDA - goal_cnt / shot_cnt
     # Print the ans.
     print(
         f'MatchID:{matchid} Huskies:{is_huskies} C:{C_ans} D:{D_ans} L1:{L1_ans} L2:{L2_ans} '
